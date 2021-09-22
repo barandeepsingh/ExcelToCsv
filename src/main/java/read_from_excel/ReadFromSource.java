5e5bdeaffa1586669ee823cc4e7941c995ea9ebc
Barandeep Singh <barandeepsingh@gmail.com>
2021-09-22 10:51:01 +0530

    Added logic for CSV source

--- src/main/java/read_from_excel/ReadFromSource.java
+++ src/main/java/read_from_excel/ReadFromSource.java
@@ -6,9 +6,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ReadFromExcel {
+public class ReadFromSource {
     public static void main(String[] args) {
-        String filePath = "/Users/baran/Documents/linux_bootcamp/input_file.xlsx";
+//        String filePathExcel = "/Users/baran/Documents/linux_bootcamp/input_file.xlsx";
+        String filePathCsv = "/Users/baran/Documents/linux_bootcamp/input_file.csv";
         String outputPath = "/Users/baran/Documents/linux_bootcamp/output_file.csv";
         String outputCsvHeader = "Col0,Col1,Col2,Col3,Col4,Col5";
         Map<Integer,Integer> mapColumns = new HashMap<>();
@@ -18,8 +19,9 @@ public class ReadFromExcel {
         mapColumns.put(5,5);
         int totalColumns = (int) Arrays.stream(outputCsvHeader.split(",")).count();
         String sheetName = "Sheet1";
-        List<String> dataFromExcel = FileUtils.getDataFromExcel(outputCsvHeader, filePath, sheetName);
-        FileUtils.writeToCsv(outputPath, dataFromExcel,mapColumns,totalColumns);
+//        List<String> dataFromExcel = FileUtils.getDataFromExcel(outputCsvHeader, filePathExcel, sheetName);
+        List<String> dataFromCsv = FileUtils.getDataFromCsv(outputCsvHeader, filePathCsv, sheetName);
+        FileUtils.writeToCsv(outputPath, dataFromCsv,mapColumns,totalColumns);
         System.out.println("Processing completed. File written at "+outputPath);
 
     }
